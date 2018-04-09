18f8c637ace93ddc441afb0f98a07a9745ccd39f
Reuben Ayres <ra@sent.com>
2018-04-09 14:50:57 -0500

    made a show route

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.get('/fruits', (req, res) => {
 	res.send(fruits);
 })
 
+// ** show ** route -- show all info about one particular fruit
+app.get('/fruits/:id', (req, res) => {
+	const index = req.params.id
+	res.send(fruits[index])
+})
+
 app.listen(3000, () => {
 	console.log("Server listening on port 3000");
 })
